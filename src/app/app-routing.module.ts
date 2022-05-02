5b8e8ac151a4fcaa8948c1ee591af991570628e5
Damir Arh <damir.arh@gmail.com>
2022-05-02 10:38:00 +0200

    Redirect caught inside iframe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SurveyComponent } from './survey/survey.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'survey', component: SurveyComponent },
+  { path: 'thank-you', component: ThankYouComponent },
+  { path: '', redirectTo: '/survey', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
